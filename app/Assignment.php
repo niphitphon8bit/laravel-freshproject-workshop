ea6417dc4960934e867260ce667326ccd35715ba
niphitphon8bit <niphitphon_nt@hotmail.com>
2020-07-21 14:33:20 +0700

    add complete function to complete assignment work

--- app/Assignment.php
+++ app/Assignment.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Assignment extends Model
 {
-    //
+    /*
+        * complete 
+        * completed assignments
+        * input : -
+        * output : completed column set to true (1)
+        * Create : 07/21/2020
+        * Author : Niphitphon
+        * Last Edit : 07/21/2020 Nipitphon
+    */
+    public function complete(){
+        $this->completed = true;
+        $this->save();
+    }
 }
