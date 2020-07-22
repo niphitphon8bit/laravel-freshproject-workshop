4b56ab6e70f863bcb0fdf2c6e72bac5df188bf05
niphitphon8bit <niphitphon_nt@hotmail.com>
2020-07-22 10:58:23 +0700

    update foreach loop for article in db

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -113,45 +113,21 @@
 						<p>In arcu accumsan arcu adipiscing accumsan orci ac. Felis id enim aliquet. Accumsan ac integer lobortis commodo ornare aliquet accumsan erat tempus amet porttitor.</p>
 					</header>
 					<div class="testimonials">
-						<section>
+						@foreach ($articles as $article)
+						<section>		
 							<div class="content">
 								<blockquote>
-									<p>Nunc lacinia ante nunc ac lobortis ipsum. Interdum adipiscing gravida odio porttitor sem non mi integer non faucibus.</p>
+									<p>{{ $article->excerpt }}</p>
 								</blockquote>
 								<div class="author">
 									<div class="image">
-										<img src="images/pic01.jpg" alt="" />
+										<img src="images/pic0{{ $loop->index + 1 }}.jpg" alt="" />
 									</div>
-									<p class="credit">- <strong>Jane Doe</strong> <span>CEO - ABC Inc.</span></p>
+									<p class="credit">- <a href="/article/{{ $article->id }}"><strong>{{ $article->title }}</strong></a> <span>CEO - ABC Inc.</span></p>
 								</div>
 							</div>
 						</section>
-						<section>
-							<div class="content">
-								<blockquote>
-									<p>Nunc lacinia ante nunc ac lobortis ipsum. Interdum adipiscing gravida odio porttitor sem non mi integer non faucibus.</p>
-								</blockquote>
-								<div class="author">
-									<div class="image">
-										<img src="images/pic03.jpg" alt="" />
-									</div>
-									<p class="credit">- <strong>John Doe</strong> <span>CEO - ABC Inc.</span></p>
-								</div>
-							</div>
-						</section>
-						<section>
-							<div class="content">
-								<blockquote>
-									<p>Nunc lacinia ante nunc ac lobortis ipsum. Interdum adipiscing gravida odio porttitor sem non mi integer non faucibus.</p>
-								</blockquote>
-								<div class="author">
-									<div class="image">
-										<img src="images/pic02.jpg" alt="" />
-									</div>
-									<p class="credit">- <strong>Janet Smith</strong> <span>CEO - ABC Inc.</span></p>
-								</div>
-							</div>
-						</section>
+						@endforeach
 					</div>
 				</div>
 			</section>
